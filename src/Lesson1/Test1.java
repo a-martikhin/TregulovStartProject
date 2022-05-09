dcb878af6c604bc3976551149a5996453d3271f8
Aleksandr Martikhin <a.martikhin@mail.ru>
2022-05-09 12:32:37 +0300

    Add Homework

--- src/Lesson1/Test1.java
+++ src/Lesson1/Test1.java
@@ -1,8 +1,11 @@
 package Lesson1;
 
+import java.sql.SQLOutput;
+
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Welcome to the first lesson!!!");
         System.out.println("Hello...");
+        System.out.println("Hello");
     }
 }
