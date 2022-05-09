d91c09a9e2054d7011889e478697f4bea0f0eccc
Aleksandr Martikhin <a.martikhin@mail.ru>
2022-05-09 12:10:29 +0300

    add new project

---
+++ src/Lesson1/Test1.java
@@ -0,0 +1,8 @@
+package Lesson1;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the first lesson!!!");
+        System.out.println("Hello...");
+    }
+}
