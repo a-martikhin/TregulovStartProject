af1c32901dc0507d5717ae1e9801e388d08a648e
Aleksandr Martikhin <a.martikhin@mail.ru>
2022-05-26 23:40:47 +0300

    Домашнее задание урока 9 выполнено и исправлено

--- src/Lesson9HW/ExerciseOne.java
+++ src/Lesson9HW/ExerciseOne.java
@@ -1,24 +1,19 @@
 package Lesson9HW;
 
 public class ExerciseOne {
+    public static void abc(){
+        String s1 = new String("ABC");
+        String s2 = new String("DEF");
+    }
 
 }
 
 class ExerciseOneTest{
     public static void main(String[] args) {
         ExerciseOne e1 = new ExerciseOne();
-        ExerciseOne e2 = new ExerciseOne();
-        ExerciseOne e3 = new ExerciseOne();
-        ExerciseOne e4 = new ExerciseOne();
-        ExerciseOne e5 = new ExerciseOne();
-        ExerciseOne e6 = new ExerciseOne();
-        ExerciseOne e7 = new ExerciseOne();
-        ExerciseOne e8 = new ExerciseOne();
-        e3 = null;
-        e4 = null;
-        e5 = null;
-        e6 = null;
-        e7 = null;
-        e8 = null;
+        ExerciseOne.abc();
+        ExerciseOne.abc();
+        String s1 = new String("ABC");
+        ExerciseOne.abc();
     }
 }
