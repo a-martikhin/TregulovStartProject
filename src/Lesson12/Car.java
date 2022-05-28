983d641110f9e0abb831cee5b198aa9005db2695
Aleksandr Martikhin <a.martikhin@mail.ru>
2022-05-29 01:53:45 +0300

    Урок 12 пройден

---
+++ src/Lesson12/Car.java
@@ -0,0 +1,28 @@
+package Lesson12;
+
+public class Car {
+    int engine;
+    int doorCount;
+
+    Car(int engine, int doorCount){
+        this.engine = engine;
+        this.doorCount = doorCount;
+    }
+}
+
+class CarTest{
+    public static void main(String[] args) {
+        Car c1 = new Car(3,4);
+        Car c2 = new Car(2,5);
+
+        if (c1.engine > c2.engine) {
+            if (c1.doorCount > c2.doorCount) {
+                System.out.println("Moshnost motora i kol-vo dverei pervoi mashini bolsse");
+            } else {
+                System.out.println("Moshnost motora pervoi mashini bolshe, no kol-vo dverei menshe");
+            }
+        } else {
+            System.out.println("Moshnost motora u pervoi mashini menshe");
+        }
+    }
+}
